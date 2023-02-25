71e76c9c341ba93324ad3c52e15ab6823e601adc
Felix Moses <felix@tacocat.com>
2023-02-25 11:54:16 -0800

    shame

--- src/build.js
+++ src/build.js
@@ -72,6 +72,7 @@ async function html() {
         el('html',
             el('head',
                 el("title", title), 
+                el('meta name="viewport" content="width=device-width, initial-scale=1"'),
                 stylesheet("res/index.css"),
                 stylesheet("res/title.css"),
                 '<link rel="preconnect" href="https://fonts.googleapis.com">',
