5719b129c60c84debb05103bb5966db6c650fa70
Felix Moses <felix@tacocat.com>
2024-05-30 17:08:12 -0700

    remove old stuff

--- src/build.js
+++
@@ -1,9 +0,0 @@
-import home_page from "./pages/home.js"
-import nocturnal_page from "./pages/nocturnal.js"
-
-const pages = [
-    home_page,
-    nocturnal_page,
-]
-
-pages.forEach(page => page())
