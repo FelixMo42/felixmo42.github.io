db84579257289dfff3e699a93d5a229d5738c847
Felix Moses <felix@tacocat.com>
2024-12-06 20:07:32 +0100

    plant intellegence

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,13 @@
-import { build_notes } from "./main.ts";
+import { build_notes, build_tacocat } from "./main.ts"
 
 async function main() {
-    await build_notes()
+    const [ cmd, ...args ] = Deno.args
+
+    if (cmd === "notes") {
+        await build_notes()
+    } else if (cmd === "tacocat") {
+        await build_tacocat(...args)
+    }
 }
 
 main()
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
 import { CONFIG } from "./config.ts";
+import { ENCODER } from "./lib/utils.ts";
 import { reset_dir } from "./lib/utils.ts";
 import { Vault } from "./lib/vault.ts";
+
+export async function build_tacocat(...albums: string[]) {
+    for (const album of albums) {
+        // load the raw album
+        const data = await fetch(`https://pix.tacocat.com/${album}`)
+        const text = (await data.text())
+            .replaceAll(`href="/`, `href="https://pix.tacocat.com/`)
+
+        // save it
+        await Deno.mkdir(`./pub/tacocat/${album}`, { recursive: true })
+        await Deno.writeFile(
+            `./pub/tacocat/${album}/index.html`,
+            ENCODER.encode(text)
+        )
+    }
+}
 
 export async function build_notes() {
     await reset_dir(CONFIG.NOTES_PUB_DIR)
@@ -8,5 +25,6 @@ export async function build_notes() {
     const vault = await new Vault().load()
 
     await vault.build("Guns: to be or not to be")
+    await vault.build("Plant Intelligence")
     await vault.build_index(vault)
 }
